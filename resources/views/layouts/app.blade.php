05b55d2e5176b919e00d485cb009d10478aa571a
Jayjaywantee Koodun <jayjaywantee.koodun@gmail.com>
2020-01-21 11:12:01 +0100

    separate views into partials

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,66 +1,17 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+<html lang="fr">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @include('partials.head')
 </head>
-
 <body>
     @include('partials.alerts')
-    <nav>
-        <div class="nav-wrapper">
-            <a href="#" class="brand-logo">easy bail</a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-
-                @guest
-                <li><a href="{{ route('login') }}">{{ __('Connexion') }}</a></li>
-                @if (Route::has('register'))
-                <li><a href="{{ route('register') }}">{{ __('Inscription') }}</a></li>
-                @endif
-                @else
-                <a id="navbarDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    {{ Auth::user()->name }} <span class="caret"></span>
-                </a>
-
-                <div aria-labelledby="navbarDropdown">
-                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                        {{ __('Deconexion') }}
-                        {{ Auth::user()->first_name}}
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                </div>
-
-                @endguest
-            </ul>
-        </div>
-    </nav>
-
-    <main class="py-4">
-
-        @yield('content')
-    </main>
+    <div class="app">
+        @include('partials.nav')
+        <main class="py-4">
+            @yield('content')
+        </main>
+        @include('partials.footer')
     </div>
-
-
+    @yield('scripts')
 </body>
-
 </html>
